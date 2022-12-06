605afe5d5bd550893df26ffd490ef15e093e704f
matteoscaringi <scaringi.matteo@icloud.com>
2022-12-06 09:42:33 +0100

    made variables static and fixed for loops

--- stadindexer/Program.cs
+++ stadindexer/Program.cs
@@ -5,30 +5,26 @@ namespace stadindexer
 {
     class Program
     {
-        private string[] steden = new string[500];
-        private string[] inwoners = new string[500];
+        private static string[] steden = new string[500];
+        private static string[] inwoners = new string[500];
         
-        private int aantalSteden = -1;
-        private int keuze;
+        private static int aantalSteden = -1;
+        private static int keuze;
 
-        private string teBewerken;
-        private string teBewerkenNaam;
-        private string bewerkInwoners;
+        private static string teBewerken;
+        private static string teBewerkenNaam;
+        private static string bewerkInwoners;
 
-        private string teVerwijderen;
+        private static string teVerwijderen;
 
-        private string teZoeken;
+        private static string teZoeken;
         static void Main()
         {
-            Program program = new Program();
-            
             Menu();
         }
 
         private static void Menu()
         {
-            Program program = new Program();
-            
             Console.Write("MENU \n" +
                           "(1) Voeg een stad toe \n" +
                           "(2) Toon alle steden \n" +
@@ -38,9 +34,9 @@ namespace stadindexer
                           "(0) Afsluiten \n" +
                           "Kies een optie: ");
 
-            program.keuze = Convert.ToInt32(Console.ReadLine());
+            keuze = Convert.ToInt32(Console.ReadLine());
 
-            switch (program.keuze)
+            switch (keuze)
             {
                 case 1:
                     VoegStadToe();
@@ -50,45 +46,46 @@ namespace stadindexer
                     break;
                 case 3:
                     Console.Write("Welke stad wil je bewerken: ");
-                    program.teBewerken = Console.ReadLine();
+                    teBewerken = Console.ReadLine();
                     Console.Write("Nieuwe naam (Leeg laten voor geen verandering): ");
-                    program.teBewerkenNaam = Console.ReadLine();
+                    teBewerkenNaam = Console.ReadLine();
                     Console.Write("Nieuwe inwoners (Leeg laten voor geen verandering): ");
-                    program.bewerkInwoners = Console.ReadLine();
+                    bewerkInwoners = Console.ReadLine();
                     
                     BewerkStad();
                     break;
                 case 4:
                     Console.Write("Welke stad wil je verwijderen: ");
-                    program.teVerwijderen = Console.ReadLine();
+                    teVerwijderen = Console.ReadLine();
                     
                     VerwijderStad();
                     break;
                 case 5:
+                    Console.Write("Welke stad wil je zoeken: ");
+                    teZoeken = Console.ReadLine();
+
                     ZoekStad();
                     break;
             }
         }
         private static void VoegStadToe()
         {
-            Program program = new Program();
-
+            aantalSteden++;
             Console.Write("Voer een stad in: ");
-            program.steden[program.aantalSteden] = Console.ReadLine();
+            steden[aantalSteden] = Console.ReadLine();
             Console.Write("Voer het aantal inwoners in: ");
-            program.inwoners[program.aantalSteden] = Console.ReadLine();
-            program.aantalSteden++; 
+            inwoners[aantalSteden] = Console.ReadLine();
+            
             Menu();
         }
         private static void ToonAlleSteden()
         {
-            Program program = new Program();
-            
-            for (int i = 0; i <= 500; i++)
+
+            for (int i = 0; i < 500; i++)
             {
-                if (program.steden[i] != null)
+                if (steden[i] != null)
                 {
-                    Console.WriteLine("{0} Inwoners: {1}", program.steden[i], program.inwoners[i]);
+                    Console.WriteLine("{0} Inwoners: {1}", steden[i], inwoners[i]);
                 }
             }
             
@@ -96,26 +93,24 @@ namespace stadindexer
         }
         private static void BewerkStad()
         {
-            Program program = new Program();
-
-            if (program.teBewerkenNaam != "")
+            if (teBewerkenNaam != "")
             {
                 for (int i = 0; i < 500; i++)
                 {
-                    if (program.steden[i] == program.teBewerken)
+                    if (steden[i] == teBewerken)
                     {
-                        program.steden[i] = program.teBewerkenNaam;
+                        steden[i] = teBewerkenNaam;
                     }
                 }
             }
 
-            if (program.bewerkInwoners != "")
+            if (bewerkInwoners != "")
             {
-                for (int i = 0; i <= 500; i++)
+                for (int i = 0; i < 500; i++)
                 {
-                    if (program.steden[i] == program.teBewerken)
+                    if (steden[i] == teBewerken)
                     {
-                        program.inwoners[i] = program.bewerkInwoners;
+                        inwoners[i] = bewerkInwoners;
                     }
                 }
             }
@@ -124,14 +119,12 @@ namespace stadindexer
         }
         private static void VerwijderStad()
         {
-            Program program = new Program();
-
-            for (int i = 0; i <= 500; i++)
+            for (int i = 0; i < 500; i++)
             {
-                if (program.steden[i] == program.teVerwijderen)
+                if (steden[i] == teVerwijderen)
                 {
-                    program.steden[i] = null;
-                    program.inwoners[i] = null;
+                    steden[i] = null;
+                    inwoners[i] = null;
                 }
             }
 
@@ -139,13 +132,11 @@ namespace stadindexer
         }
         private static void ZoekStad()
         {
-            Program program = new Program();
-
-            for (int i = 0; i <= 500; i++)
+            for (int i = 0; i < 500; i++)
             {
-                if (program.steden[i] == program.teZoeken)
+                if (steden[i] == teZoeken)
                 {
-                    Console.WriteLine("{0} Inwoners: {1}", program.steden[i], program.inwoners[i]);
+                    Console.WriteLine("{0} Inwoners: {1}", steden[i], inwoners[i]);
                 }
             }
 
